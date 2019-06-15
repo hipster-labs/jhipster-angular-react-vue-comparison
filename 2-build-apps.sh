954128735d82fcd70f6ca931d537cb9056ddd0e0
Pierre Besson <pbesson@ippon.fr>
2019-06-15 18:14:58 +0200

    update script

--- 2-build-apps.sh
+++ 2-build-apps.sh
@@ -1,6 +1,19 @@
 # Package apps
 apps=("angular" "react" "vuejs")
+
+# Build docker images
 for app in "${apps[@]}";
 do
     ( cd $app && mvn package -Pprod -DskipTests jib:dockerBuild ) 
 done
+
+
+# Tag and push docker images
+#dockerRepositoryName="pbesson"
+#tag="6.1.0"
+# for app in "${apps[@]}";
+# do
+#     docker tag $app $dockerRepositoryName/$app:$tag
+#     docker push $dockerRepositoryName/$app:$tag
+# done
+
