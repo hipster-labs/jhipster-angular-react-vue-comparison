42a6edadf31b3d737cb5a7480839b0068133d4cd
Pierre Besson <pbesson@ippon.fr>
2019-06-17 10:13:08 +0200

    add metrics report

--- performance-tests/index.js
+++ performance-tests/index.js
@@ -8,10 +8,57 @@ const fs = require('fs');
 const request = require('request');
 const util = require('util');
 
-const openHomePage = async (browser, url) => {
+const generateMetricsReport = async (browser, url, reportName, reportColor) => {
     const page = await browser.newPage();
     await page.goto(url);
     await page.screenshot({path: `reports/${reportName}-screenshot.png`});
+
+    const rawMetrics = await page.evaluate(() => JSON.stringify(window.performance.timing));
+    const metrics = JSON.parse(rawMetrics);
+
+    const metricsReport = `<html>
+    <style>body {background-color: ${reportColor}}</style>
+    <body>
+    <h1>${reportName} metrics report</h1>
+    <table>
+    <tbody>
+        <tr>
+            <td>DNS lookup</td>
+            <td>${metrics.domainLookupEnd - metrics.domainLookupStart} ms</td>
+        </tr>
+        <tr>
+            <td>TCP connection</td>
+            <td>${metrics.connectEnd - metrics.connectStart} ms</td>
+        </tr>
+        <tr>
+            <td>Request time</td>
+            <td>${metrics.responseStart - metrics.requestStart} ms</td>
+        </tr>
+        <tr>
+            <td>Response time </td>
+            <td>${metrics.responseEnd - metrics.responseStart} ms</td>
+        </tr>
+        <tr>
+            <td>DOM load</td>
+            <td>${metrics.domComplete - metrics.domLoading} ms</td>
+        </tr>
+        <tr>
+            <td>DOM interactive</td>
+            <td>${metrics.domInteractive - metrics.navigationStart} ms</td>
+        </tr>
+        <tr>
+            <td>Document load</td>
+            <td>${metrics.loadEventEnd - metrics.loadEventStart} ms</td>
+        </tr>
+        <tr>
+            <td>Full load time</td>
+            <td>${metrics.loadEventEnd - metrics.navigationStart} ms</td>
+        </tr>
+    </tbody>
+    </table>
+    </body></html>`
+
+    fs.writeFileSync(`reports/${reportName}-metrics-report.html`, metricsReport);
 }
 
 const generateLightHouseReport = async (url, opts, reportName, reportColor) => {
@@ -25,26 +72,31 @@ const generateLightHouseReport = async (url, opts, reportName, reportColor) => {
 }
 
 (async () => {
+    const reports = [
+        {
+            url: 'http://localhost:8080',
+            name: 'angular',
+            backgroundColor: '#f9586a42',   
+        },
+        {
+            url:  'http://localhost:8081',
+            name:  'react',
+            backgroundColor:  '#60dbfb78',
+        },
+        {
+            url:  'http://localhost:8082',
+            name:  'vuejs',
+            backgroundColor:  '#0cce6bab',
+        }
+    ]
+
     const browser = await puppeteer.launch({headless: false, defaultViewport: null});
     const lighthouseOptions = { logLevel: 'info', output: 'json', port: new URL(browser.wsEndpoint()).port}
 
-    url = 'http://localhost:8080';
-    reportName = 'angular';
-    reportColor = '#f9586a42';
-    await openHomePage(browser, url);
-    const angularReport = await generateLightHouseReport(url, lighthouseOptions, reportName, reportColor)
-
-    url = 'http://localhost:8081';
-    reportName = 'react';
-    reportColor = '#60dbfb78';
-    await openHomePage(browser, url);
-    const reactReport = await generateLightHouseReport(url, lighthouseOptions, reportName, reportColor)
-    
-    url = 'http://localhost:8082';
-    reportName = 'vuejs';
-    reportColor = '#0cce6bab';
-    await openHomePage(browser, url);
-    const vuejsReport = await generateLightHouseReport(url, lighthouseOptions, reportName, reportColor)
+    for (report of reports) {
+        await generateMetricsReport(browser, report.url, report.name, report.backgroundColor);
+        await generateLightHouseReport(report.url, lighthouseOptions, report.name, report.backgroundColor)
+    }
 
     await browser.close();
 })();
