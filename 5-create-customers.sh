6521499d2fd080b6a4a3401d486fc2f0dca8754b
Pierre Besson <pbesson@ippon.fr>
2019-06-25 17:57:09 +0200

    add fake data the good old way

---
+++ 5-create-customers.sh
@@ -0,0 +1,15 @@
+apps=("localhost:8080" "localhost:8081" "localhost:8082")
+
+# Build docker images
+for app in "${apps[@]}";
+do
+  for run in {1..1000};
+  do
+    curl -X POST \
+    --header 'Content-Type: application/json' \
+    --header 'Accept: application/json' \
+    -d '{"addressLine1": "string", "addressLine2": "string", "city": "string","country": "string","email": "string@string.fr", "firstName": "string","gender": "MALE","lastName": "string","phone": "string"}' \
+    "http://${app}/api/customers"
+  done 
+done
+
